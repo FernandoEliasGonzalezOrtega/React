7ce84b1d057a4cfc88222795a94ef9b3e977d339
FernandoEliasGonzalezOrtega <78108804+FernandoEliasGonzalezOrtega@users.noreply.github.com>
2022-05-04 16:30:05 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import "./bootstrap.min.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [name, updName] = useState(0);
+  const updateData = () => {
+    try {
+      updName(name + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Contador</h1>
+      <h2>Empieza a contar!!!</h2>
+      <p>{name}</p>
+      <button class="btn btn-primary" onClick={updateData}>
+        Actualizar
+      </button>
+    </div>
+  );
+}
